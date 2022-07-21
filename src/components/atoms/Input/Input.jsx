9282160adeea96e93ea29be375b20d0b134fd683
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-07-21 17:55:45 +0200

    Add Input component and tests

---
+++ src/components/atoms/Input/Input.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Input = ({ type, name, value, disabled }) => {
+  return <input type={type} name={name} value={value} disabled={disabled} />;
+};
+
+Input.propTypes = {
+  type: PropTypes.oneOf([
+    'button',
+    'checkbox',
+    'color',
+    'date',
+    'datetime-local',
+    'email',
+    'file',
+    'hidden',
+    'image',
+    'month',
+    'number',
+    'radio',
+    'range',
+    'reset',
+    'search',
+    'submit',
+    'tel',
+    'text',
+    'time',
+    'url',
+    'week'
+  ]),
+  name: PropTypes.string.isRequired,
+  value: PropTypes.instanceOf(Object),
+  disabled: PropTypes.bool
+};
+
+Input.defaultProps = {
+  type: 'text',
+  value: null,
+  disabled: false
+};
+
+export default Input;
