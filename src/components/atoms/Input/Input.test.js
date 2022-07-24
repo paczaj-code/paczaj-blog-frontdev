e0885c9a226ef3db2467a5f7ded8900d4b12f4a0
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-07-24 13:11:08 +0200

    Add tests

--- src/components/atoms/Input/Input.test.js
+++ src/components/atoms/Input/Input.test.js
@@ -6,64 +6,87 @@ const input_types = [
   'button',
   'checkbox',
   'color',
-  // 'date',
-  // 'datetime-local',
   'email',
-  'file',
   'hidden',
-  // 'image',
-  // 'month',
-  // 'number',
+  'password',
   'radio',
   'range',
   'reset',
-  // 'search',
   'submit',
-  // 'tel',
   'text',
-  // 'time',
-  'url'
-  // 'week'
+  'url',
 ];
 // it should be proper type with type prop and text type without
-describe('it should be proper input type with type prop and text type without', () => {
+describe('tests for input attributes but class', () => {
 
   // test('it shuold be input type text with type prop ', () => {
   //   const { container } = render(<Input name={input_name} />);
   //   expect(container.firstChild).toHaveAttribute('type', 'text');
   // });
 
-  // for (let input_type in input_types) {
-  test(`it shold be input type text with type text  `, () => {
-    const { container } = render(
-      <Input type='text' name={input_name} onChange={() => { }} value="some value" />
-    );
-    expect(container.firstChild).toHaveAttribute('type', 'text');
+  for (let input_type of input_types) {
+    test(`it shold be input type text with type ${input_type}  `, () => {
+      const { container } = render(
+        <Input type={input_type} name={input_name} onChange={() => { }} value="some_value" />
+      );
+      expect(container.firstChild).toHaveAttribute('type', input_type);
+    });
+  }
+
+
+  // it should be proper name with name prop
+  test('it should be proper name with name prop', () => {
+    const { container } = render(<Input name={input_name} onChange={() => { }} value="some_value" />);
+    expect(container.firstChild).toHaveAttribute('name', input_name);
   });
-  // }
+
+  // // it should be proper value with value prop
+  test('it should be proper value with value prop', () => {
+    const input_value = 'some_value';
+    const { container } = render(<Input name={input_name} value={input_value} onChange={() => { }} />);
+    expect(container.firstChild).toHaveAttribute('value', input_value);
+  });
+
+  // // it should be disabled  with disabled prop
+  test('it should be disabled  with disabled prop', () => {
+    const { container } = render(<Input name={input_name} disabled={true} onChange={() => { }} value="some_value" />);
+    expect(container.firstChild).toHaveAttribute('disabled');
+  });
+
+  // // it should not be disabled  without disabled prop
+  test(' it should not be disabled  without disabled prop', () => {
+    const { container } = render(<Input name={input_name} disabled={false} onChange={() => { }} value="some_value" />);
+    expect(container.firstChild).not.toHaveAttribute('disabled');
+  });
+
+
 });
 
-// it should be proper name with name prop
-// test('it should be proper name with name prop', () => {
-//   const { container } = render(<Input name={input_name} onChange={() => { }} />);
-//   expect(container.firstChild).toHaveAttribute('name', input_name);
-// });
+describe('tests for input classes', () => {
+  // it should be deafult class withou any class related props
+  test(' it should  be default input--medium--primary class without any class related props', () => {
+    const { container } = render(<Input name={input_name} disabled={false} onChange={() => { }} value="some_value" />);
+    expect(container.firstChild).toHaveClass('input--medium--primary');
+  });
 
-// // it should be proper value with value prop
-// test('it should be proper value with value prop', () => {
-//   const input_value = 'some_value';
-//   const { container } = render(<Input name={input_name} value={input_value} onChange={() => { }} />);
-//   expect(container.firstChild).toHaveAttribute('value', input_value);
-// });
+  const sizes = ['small', 'medium', 'big'];
+  for (let size of sizes) {
+    test(`it should be input--${size}--primary class with  ${size} as size prop`, () => {
+      const { container } = render(<Input name={input_name} size={size} value="some_value" onChange={() => { }} />);
+      expect(container.firstChild).toHaveClass(`input--${size}--primary`);
+    })
+  }
 
-// // it should be disabled  with disabled prop
-// test('it should be disabled  with disabled prop', () => {
-//   const { container } = render(<Input name={input_name} disabled={true} onChange={() => { }} />);
-//   expect(container.firstChild).toHaveAttribute('disabled');
-// });
+  const colors = ['primary', 'secondary', 'success', 'danger'];
+  for (let color of colors) {
+    test(`it should be input--medium--${color} class with  ${color} as color prop`, () => {
+      const { container } = render(<Input name={input_name} color={color} value="some_value" onChange={() => { }} />);
+      expect(container.firstChild).toHaveClass(`input--medium--${color} `);
+    })
+  }
 
-// // it should not be disabled  without disabled prop
-// test(' it should not be disabled  without disabled prop', () => {
-//   const { container } = render(<Input name={input_name} disabled={false} onChange={() => { }} />);
-//   expect(container.firstChild).not.toHaveAttribute('disabled');
-// });
+  test(`it should be  prefix with class_prefix prop`, () => {
+    const { container } = render(<Input name={input_name} value="some_value" onChange={() => { }} class_prefix='login' />);
+    expect(container.firstChild).toHaveClass(`login__input--medium--primary `);
+  })
+})
---
+++ src/components/atoms/Link/Link.test.js
@@ -0,0 +1,9 @@
+
+
+test(`it shold be input type text with type text  `, () => {
+    // const { container } = render(
+    //   <Input type='text' name={input_name} onChange={() => { }} value="some value" />
+    // );
+    // expect(container.firstChild).toHaveAttribute('type', 'text');
+    expect(3).toEqual(3)
+});
