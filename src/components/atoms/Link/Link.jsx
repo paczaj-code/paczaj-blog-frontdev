c532dfb10f12a82a496869e1c629ab7eddb9d76a
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-07-24 13:24:19 +0200

    Refactor Link component

--- src/components/atoms/Link/Link.jsx
+++ src/components/atoms/Link/Link.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Link = ({ href }) => {
-  // return <a href={href ? href : 'google.com'}></a>;
+const Link = ({ href, size, color, class_prefix }) => {
+  return (
+    <a
+      href={href}
+      className={
+        class_prefix
+          ? `${class_prefix}__link--${size}--${color}`
+          : `link--${size}--${color}`
+      }
+    ></a>
+  );
+};
+
+Link.propTypes = {
+  href: PropTypes.string.isRequired,
+  size: PropTypes.oneOf(['small', 'medium', 'big']),
+  color: PropTypes.oneOf(['primary', 'secondary', 'success', 'danger']),
+  class_prefix: PropTypes.string
+};
+
+Link.defaultProps = {
+  size: 'medium',
+  color: 'primary',
+  class_prefix: null
 };
 
 export default Link;
